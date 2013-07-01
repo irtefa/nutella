7f4ca64cc10359dfe1722696a454336ae5b4f7b5
Mohd Irtefa <irtefa1@illinois.edu>
2013-06-30 17:47:39 -0700

    configured initial Gemfile reqs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nutella::Application.config.secret_key_base = '87f52c175f5c0257e4f8660d91abef6c438857908dfff9eeac4bab378bffa339a99867e579fca91d367a000a04ba71f0cdda7cb48f672d9286f5a3292acea02f'
