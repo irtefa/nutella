e4aa6e34c86e0ac44052c6e9053ca4ad199842de
Mohd Irtefa <irtefa1@illinois.edu>
2013-07-03 13:08:43 -0700

    added user model with email and passwd

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                            format: { with: VALID_EMAIL_REGEX },
+                            uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6}
+  before_save { self.email = email.downcase }
+end
